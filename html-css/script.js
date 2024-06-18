441f507d2905683900a981be9f340f3f53084407
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-18 16:03:32 +0900

    emailとpassword自動入力機能完成

--- html-css/script.js
+++ html-css/script.js
@@ -1,9 +1,22 @@
-    // サインインボタンクリック時の処理
-    document.getElementById('signinButton').addEventListener('click', () => {
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+// ページが読み込まれたときの処理
+window.addEventListener('load', () => {
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
 
-        // メールアドレスをLocalStorageに保存
-        localStorage.setItem('email', email);
-        localStorage.setItem('password', password);
-    });
+    if (storedEmail) {
+        document.getElementById('email').value = storedEmail;
+    }
+    if (storedPassword) {
+        document.getElementById('password').value = storedPassword;
+    }
+});
+
+// サインインボタンクリック時の処理
+document.getElementById('signinButton').addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // メールアドレスとパスワードをLocalStorageに保存
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+});
